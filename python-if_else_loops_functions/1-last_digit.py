14b835c41fd58179ac5eea3a9fea122d8b691c8c
Amalshfyv <amalshafiyev07@gmail.com>
2026-03-17 11:56:06 +0000

    submitting

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-a = str(number)
-print(a[-1])
+a = str(number)[-1]
+if int(a) > 5:
+    print(f"{a} is greater than 5")
+elif int(a) == 0:
+    print(f"{a} is 0")
+elif 0 < int(a) < 6:
+    print(f"{a} is less than 6 and not 0")
+
+
